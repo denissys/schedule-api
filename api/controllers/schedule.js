ad05c4df1f978b00887ff82d46b316c3afe41247
denissys <denissys@gmail.com>
2018-01-03 14:14:24 -0200

    Design of API

--- api/controllers/schedule.js
+++ api/controllers/schedule.js
@@ -3,12 +3,21 @@
 var util = require('util');
 
 module.exports = {
-  schedule: schedule
+  getSchedule: getSchedule,
+  addSchedule: addSchedule
 };
 
-function schedule(req, res) {
+function getSchedule(req, res) {
 
   var sellerId = req.query.sellerId;
   var msgRes = util.format('Schedule, %s', sellerId);
   res.json(msgRes);
+}
+
+function addSchedule(req, res) {
+
+  //var sellerId = req.query.sellerId;
+  var Schedule = {"sellerId": "123"};
+  res.header('Content-Type', 'application/json');
+  res.json(Schedule);
 }
