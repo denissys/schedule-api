0a9d39e68fc7f5a2af197ecc60b5c5117b74d80e
denissys <denissys@gmail.com>
2018-01-05 12:36:29 -0200

    Configure CORS

--- app.js
+++ app.js
@@ -21,6 +21,12 @@ SwaggerExpress.create(config, function(err, swaggerExpress) {
   app.use(SwaggerUi(swaggerExpress.runner.swagger));
   app.use(bodyParser.json())
   app.use(bodyParser.urlencoded({extended: true}))
+  app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+  });
+
 
   // install middleware
   swaggerExpress.register(app);
